f86da132d2d83ffcf15af1a9f495ae868195fb9c
Rafael Grazzini Placucci <rafael@placucci.com.br>
2025-10-17 21:46:46 +0200

    Add utils for tokenization and postprocessing

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+def tokenize_fn(example, task_name, tokenizer):
+    if task_name == "agnews":
+        return tokenizer(example["content"], truncation=True)
+    elif task_name == "dbpedia":
+        return tokenizer(example["text"], truncation=True)
+    else:
+        raise NotImplementedError(f"Task '{task_name}' is not supported")
+
+def postprocess_fn(dataset, task_name):
+    columns_to_remove = []
+    if task_name == "agnews":
+        columns_to_remove.extend(["title", "content"])
+    elif task_name == "dbpedia":
+        columns_to_remove.extend(["text"])
+    else:
+        raise NotImplementedError(f"Task '{task_name}' is not supported")
+    
+    dataset = dataset.rename_column("label", "labels")
+    dataset = dataset.remove_columns(columns_to_remove)
+    dataset.set_format("torch")
+    return dataset
