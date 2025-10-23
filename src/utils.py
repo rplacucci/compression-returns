e7af44b082dd7e077771d7388cc0d96a5087e457
Rafael Grazzini Placucci <rafael@placucci.com.br>
2025-10-23 16:09:12 +0200

    Fix minor bugs

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,16 @@
 def tokenize_fn(example, task_name, tokenizer):
-    if task_name == "ag_news":
+    if task_name == "dbpedia_14":
         return tokenizer(example["content"], truncation=True)
-    elif task_name == "dbpedia_14":
+    elif task_name == "ag_news":
         return tokenizer(example["text"], truncation=True)
     else:
         raise NotImplementedError(f"Task '{task_name}' is not supported")
 
 def postprocess_fn(dataset, task_name):
     columns_to_remove = []
-    if task_name == "ag_news":
+    if task_name == "db_pedia":
         columns_to_remove.extend(["title", "content"])
-    elif task_name == "dbpedia_14":
+    elif task_name == "ag_news":
         columns_to_remove.extend(["text"])
     else:
         raise NotImplementedError(f"Task '{task_name}' is not supported")
